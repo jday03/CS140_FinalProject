3bd2edc751e314a5d5cf425483a8ac02f95abd32
jday03 <E!n$t31n>
2018-03-12 19:06:22 -0700

    more work on reducer and bag

--- bag.cpp
+++ bag.cpp
@@ -3,34 +3,52 @@
 //
 
 #include "bag.h"
+bag::bag(){
 
+
+}
 bag bag::bagSplit() {
     bag returnBag;
+
+    pennant * newPennant1;
+    returnBag.data.insert(returnBag.data.end(),newPennant1 );
 
     pennant * first= data[0];
     data[0] = nullptr;
 
     for( int i = 1; i < data.size(); ++i){
-        if(data[i] != nullptr){
-            *(returnBag.data[i-1]) = data[i]->pennantSplit();
-            data[i-1] = data[i];
+        if(data[i] != nullptr) {
+            if (i >= returnBag.data.size()) {
+                pennant * newPennant;
+                returnBag.data.insert(returnBag.data.end(),newPennant );
+            }
+            pennant penn = data[i]->pennantSplit();
+            *(returnBag.data[i - 1]) = penn;
+            data[i - 1] = data[i];
             data[i] = nullptr;
         }
 
-    }
+        }
+
+
     if(first != nullptr){
-        insert(first->root); //BAG INSERT FIRST
+        insertNode(first->getRoot()); //BAG INSERT FIRST
     }
 
-    return ;
+    return returnBag;
 }
 
-void bag::insertPennant(pennant insert) {
-
-
-}
 
 void bag::insertNode(node insert) {
-    
+    int count = 0;
+    // merger must contain insert!!
+    pennant merger;
+    while (data[count] != nullptr){
+        merger = merger.pennantUnion(merger,*data[count]);
+        data[count] = nullptr;
+        count++;
+    }
 
+    *(data[count]) = merger;
 }
+
--- pennant.h
+++ pennant.h
@@ -5,6 +5,10 @@ struct node
 int from, to;
     node *left;
     node *right;
+    node(int fr, int too){
+        from = fr;
+        to = too;
+    }
 };
 
 class pennant {
@@ -14,7 +18,7 @@ public:
     pennant pennantUnion(pennant x, pennant y);
     pennant pennantSplit(pennant& x);
     pennant pennantSplit();
-
+    node getRoot();
 
 private:
     node *root;
