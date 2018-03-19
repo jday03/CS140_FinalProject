4224c6d5f229a4bf17b0d4b94bae622633732465
Jonathan Day <jday.day03@gmail.com>
2018-03-19 00:34:08 -0700

    started work on BFS

---
+++ BFS.cpp
@@ -0,0 +1,17 @@
+void BFS(Graph *G, Vertex root)
+{
+Bag<Vertex> frontier(root);
+while ( ! frontier.isEmpty() )
+{
+cilk::hyperobject< Bag<Vertex> > succbag();
+cilk_for (int i=0; i< frontier.size(); i++)
+{
+for( Vertex v in frontier[i].adjacency() )
+{
+if( ! v.unvisited() )
+succbag.insert(v);
+}
+}
+frontier = succbag.getValue();
+}
+}
