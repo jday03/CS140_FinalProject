84ea494fcda571e89956a1216e4bcef168b5a4dd
noelvargas <noelleon216@gmail.com>
2018-03-19 20:22:24 -0700

    created main function

--- main.cpp
+++ main.cpp
@@ -5,7 +5,10 @@
 #include <cilk/reducer.h>
 #include "pennant.h"
 #include "bag.h"
-
+#include <fstream>
+#include <numeric>
+#include <cmath>
+#include "example_util_gettime.h"
 
 class BagMonoid;
 
@@ -103,13 +106,13 @@ void printDepthCounter(std::map<int, std::vector<int>> depthMap ){
 
 
 
-void BFS(std::vector<node> graph, node root)
+std::map<int, std::vector<int> > BFS(std::vector<node> graph, node root)
 {
     root.visited = true;
     node* ptr = &root;
     bag frontier;
     frontier.insertNode(ptr);
-    std::map<int, std::vector<int>> depthMap;
+    std::map<int, std::vector<int> > depthMap;
     int depthCounter = -1;
 
     while ( ! frontier.isEmpty() )
@@ -133,6 +136,7 @@ void BFS(std::vector<node> graph, node root)
     }
 
     manageDepthCounter(depthCounter,graph,depthMap);
+    return depthMap;
 }
 
 
@@ -142,9 +146,41 @@ void BFS(std::vector<node> graph, node root)
 
 
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
-    cilk::hyperobject< Bag<int> > succbag();
+int main(int argc, char **argv) {
+    long t1, t2;
+    //cilk::hyperobject< Bag<int> > succbag();
+    if(argc < 2)
+    {
+        printf("Usage : ./pbfs <graphinputfile> \n");
+        exit(-1);
+    }
+    std::vector<node> graph;
+    bag Bag;
+
+    std::ifstream inFile;
+    inFile.open(argv[1], std::ifstream::in);
+
+    int temp1,  temp2;
+    while(inFile.good()){
+        temp1 = inFile.get();
+        temp2 = inFile.get();
+        //add vertexes to graph
+        insertEdge(graph,temp2,temp1);
+    }
+
+    std::map<int, std::vector<int> > depthMap;
+
+    srand(time(NULL));
+    t1 = example_get_time();
+
+    depthMap = BFS(graph, *Bag.data[0]->root);
+
+    t2 = example_get_time();
+    printDepthCounter(depthMap);
+
+    std::cout << "Time: " << t2 - t1 << std::endl;
+    return 0;
+
 
 }
 
